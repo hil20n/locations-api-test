ec5e70b938b6f500a935a6d92439c7da6e0bea7c
hil20n <johhilden@gmail.com>
2021-09-09 15:30:19 +0300

    sijaintia

--- index.js
+++ index.js
@@ -11,13 +11,35 @@ app.get("/customers", (req, res) => {
   res.send(nimi);
 });*/
 
+// indentation is set to 2 spaces
+var str = JSON.stringify({ id: 1, latitude: 60, longitude: 70 }, null, 3);
+
 let database = [
   { id: 1, latitude: 60, longitude: 70 },
   { id: 2, latitude: 40, longitude: 80 },
 ];
 
-app.get("/customers", (req, res) => {
+app.get("/locations", (req, res) => {
   res.send(database);
+});
+
+app.get("/locations/:myId([0-9]+)", (req, res) => {
+  let id = req.params.myId;
+  for (let i = 0; i < database.length; i++) {
+    if (database[i].id == id) {
+      locations = database[i];
+    }
+  }
+  if ((locations = !null)) {
+    res.send(locations);
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+app.get("api/locations/:urlId([0-9]+)", (req, res) => {
+  const urlId = Number(req.params.urlId);
 });
 
 app.listen(port, () => {
