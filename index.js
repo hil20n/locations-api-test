a9310259172c3128f8c8ccaaf7ef858225097622
hil20n <johhilden@gmail.com>
2021-09-09 14:57:15 +0300

    update more and more

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const port = process.env.PORT || 8080;
 // serve the public directory as a static content here
 app.use(express.static("public"));
 
+/*let nimi = "fetch all locations";
+app.get("/customers", (req, res) => {
+  res.send(nimi);
+});*/
+
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 },
+];
+
+app.get("/customers", (req, res) => {
+  res.send(database);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
